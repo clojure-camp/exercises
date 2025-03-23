aadc0719e7450e9c6d734dc5a5667b84dcb4b930
Rafal Dittwald <rafal.dittwald@gmail.com>
2025-03-23 12:56:35 -0400

    Add backlog of exercises

---
+++ inbox/sum2.clj
@@ -0,0 +1,30 @@
+
+
+(let [coll [1 2 5 6 7]
+      target 11]
+  (first (for [a coll
+               b coll
+               :when (and
+                       (= target (+ a b))
+                       (< a b))]
+           [a b])))
+
+(let [coll [1 2 5 6 7]
+      target 11]
+  (loop [coll coll]
+    (case (compare target
+                   (+ (first coll) (last coll)))
+      0 [(first coll) (last coll)]
+      1 (recur (rest coll))
+      -1 (recur (butlast coll)))))
+
+(let [coll [1 2 5 6 7]
+      target 11]
+  (loop [l 0
+         r (dec (count coll))]
+    (case (compare target
+                   (+ (coll l) (coll r)))
+      0 [(coll l) (coll r)]
+      1 (recur (inc l) r)
+      -1 (recur l (dec r)))))
+
